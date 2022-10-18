30a50c47167774f4a4b8db7050d0c552c503048e
jj19100 <1910026791@qq.com>
2022-10-18 09:55:02 +0800

    init

---
+++ service1.js
@@ -0,0 +1,8 @@
+const Koa = require('koa');
+const app = new Koa();
+const serve = require('koa-static');
+const path = require('path');
+
+const home = serve(path.join(__dirname, '/public3/'));
+app.use(home);
+app.listen(3000);
